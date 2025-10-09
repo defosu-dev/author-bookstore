c00659de12bbb99d3faccbbe4786cd03f5fcd1bc
defosu-dev <rikyamorhun@gmail.com>
2025-10-09 15:59:06 +0300

    feat: enhance home page with subscription section and social media icons
    - Add a new subscription section to the home page for user email sign-up.
    - Introduce new social media icons including Instagram.
    - Update globals.css with new color and tracking variables for improved styling.
    - Modify existing icon components to use currentColor for better flexibility.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,6 +6,8 @@ import IconTwitter from "@/components/icons/social/IconTwitter";
 import IconLinkedin from "@/components/icons/social/IconLinkedin";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
+import IconInstagram from "@/components/icons/social/IconInstagram";
+import IconThreeDots from "@/components/icons/IconThreeDots";
 
 export default function Home() {
   return (
@@ -13,6 +15,7 @@ export default function Home() {
       <section className="px-4 bg-secondary">
         {/* Header Section */}
         <header className="container mx-auto max-w-[1300px] flex justify-between items-center py-[22px]">
+          {/* Logo and Social Links */}
           <div className="space-x-9 flex items-center">
             <Link href="/">
               <IconLogo />
@@ -1083,6 +1086,148 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <section className="bg-white px-4">
+        <div className="container mx-auto max-w-[1300px] py-[120px]">
+          <div className="pt-[100px] pb-[115px] bg-primary px-4 flex flex-col justify-between items-center shadow-lg rounded-[5px]">
+            {/* Title Section */}
+            <div className="flex flex-col gap-[23px] items-center">
+              <h2 className="text-secondary font-family-cardo text-size-42 leading-50-4 tracking-0 ">
+                Get The Best Updates
+              </h2>
+              <div className="bg-secondary w-[50px] h-[3px]" />
+            </div>
+            <p className="text-center text-size-18 leading-28-5 tracking-0 font-family-inter mt-[25px] text-secondary">
+              Subscribe with your mail id to know the current update in book
+              <br /> edition at your fingertip with in a minute.
+            </p>
+            <form
+              action="#"
+              className="flex gap-4 items-center justify-center w-full mt-[49px]"
+            >
+              <input
+                type="email"
+                placeholder="Your Email ID..."
+                className="w-full h-[70px] px-[39px] pt-[19px] pb-[20px] border border-denary max-w-[656px] bg-white placeholder-quinary text-black"
+              />
+              <button className="bg-secondary w-full flex justify-center items-center max-w-[328px] h-[70px]">
+                <span className="text-size-18 leading-30-4 tracking-0-18 text-white font-family-inter">
+                  Subscribe
+                </span>
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
+      <footer className="bg-secondary px-4">
+        <div className="container mx-auto max-w-[1300px] pt-[92px] pb-[25px] flex flex-col gap-[95px]">
+          <div className="flex">
+            <div className="flex flex-col gap-5">
+              <Link href="/">
+                <IconLogo />
+              </Link>
+
+              <div
+                className={cn(
+                  "space-x-2.5 flex items-center",
+                  "[&>a]:w-11 [&>a]:h-11 [&>a]:bg-transparent [&>a]:text-white [&>a]:flex [&>a]:items-center [&>a]:justify-center [&>a]:aspect-square [&>a]:border [&>a]:border-primary"
+                )}
+              >
+                <Link href="/">
+                  <IconFacebook />
+                </Link>
+                <Link href="/">
+                  <IconTwitter />
+                </Link>
+                <Link href="/">
+                  <IconLinkedin />
+                </Link>
+                <Link href="/">
+                  <IconInstagram />
+                </Link>
+              </div>
+            </div>
+            <div className="ml-[88px] flex flex-col gap-6">
+              <h3 className="text-white text-2xl leading-33-6 tracking-0-38 font-family-cardo">
+                Explore
+              </h3>
+              <nav className="pl-[11px] flex flex-col gap-[9px] [&>a]:flex [&>a]:items-center [&>a]:gap-[9px] [&>a]:text-size-18 [&>a]:leading-30-6 [&>a]:tracking-0-18 [&>a]:font-family-inter [&>a]:text-quinary">
+                <Link href="/">
+                  <IconThreeDots className="text-[#FFCA42]" />
+                  Home
+                </Link>
+                <Link href="/">
+                  <IconThreeDots className="text-[#FFCA42]" />
+                  About
+                </Link>
+                <Link href="/">
+                  <IconThreeDots className="text-[#FFCA42]" />
+                  Articles
+                </Link>
+                <Link href="/">
+                  <IconThreeDots className="text-[#FFCA42]" />
+                  Our Store
+                </Link>
+                <Link href="/">
+                  <IconThreeDots className="text-[#FFCA42]" />
+                  Contact Us
+                </Link>
+              </nav>
+            </div>
+            <div className="ml-[100px] flex flex-col gap-6">
+              <h3 className="text-white text-2xl leading-33-6 tracking-0-38 font-family-cardo">
+                Utility Pages
+              </h3>
+              <nav className="pl-[11px] flex flex-col gap-[9px] [&>a]:flex [&>a]:items-center [&>a]:gap-[9px] [&>a]:text-size-18 [&>a]:leading-30-6 [&>a]:tracking-0-18 [&>a]:font-family-inter [&>a]:text-quinary">
+                <Link href="/">
+                  <IconThreeDots className="text-[#FFCA42]" />
+                  Style Guide
+                </Link>
+                <Link href="/">
+                  <IconThreeDots className="text-[#FFCA42]" />
+                  404 Not Found
+                </Link>
+                <Link href="/">
+                  <IconThreeDots className="text-[#FFCA42]" />
+                  Password Protected
+                </Link>
+                <Link href="/">
+                  <IconThreeDots className="text-[#FFCA42]" />
+                  Licenses
+                </Link>
+                <Link href="/">
+                  <IconThreeDots className="text-[#FFCA42]" />
+                  Changelog
+                </Link>
+              </nav>
+            </div>
+            <div className="ml-[93px] flex flex-col">
+              <h3 className="text-white text-2xl leading-33-6 tracking-0-38 font-family-cardo">
+                Keep in Touch
+              </h3>
+              <div className="flex gap-7 mt-[27px]">
+                <div className=" flex flex-col gap-7 [&>*]:text-white [&>*]:font-family-cardo [&>*]:text-size-18 [&>*]:leading-27 [&>*]:tracking-0-36 ">
+                  <div>Address :</div>
+                  <div>Mail :</div>
+                  <div>Phone :</div>
+                </div>
+                <div className=" flex flex-col gap-7 [&>*]:text-quinary [&>*]:font-family-inter [&>*]:text-size-17 [&>*]:leading-27 [&>*]:tracking-0 ">
+                  <div>
+                    24A Kingston St, Los Vegas <br /> NC 28202, USA.
+                  </div>
+                  <div>support@pages.com</div>
+                  <div>(+22) 123 - 4567 - 900</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <p className="text-center text-quinary text-size-17 leading-25-5 tracking-0-17 font-family-inter [&>a]:text-white">
+              © Drafted by <Link href="/">VictorFlow</Link> - Powered by Link{" "}
+              <Link href="/">Webflow</Link>.
+            </p>
+          </div>
+        </div>
+      </footer>
     </main>
   );
 }
