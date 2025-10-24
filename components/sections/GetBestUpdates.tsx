d9ccbd0deead2fd03a5603b47c825f762722d401
defosu-dev <defosu.33@gmail.com>
2025-10-24 18:04:05 +0300

    fix: refine layout and spacing in GetBestUpdates component for improved consistency

--- components/sections/GetBestUpdates.tsx
+++ components/sections/GetBestUpdates.tsx
@@ -7,7 +7,7 @@ export default function GetBestUpdates() {
   return (
     <section className="bg-white px-4">
       <div className="container mx-auto max-w-[1300px] py-[120px]">
-        <div className="pt-[100px] pb-[115px] bg-primary px-4 flex flex-col justify-between items-center shadow-lg rounded-[5px]">
+        <div className="pt-[96px] pb-[116px] bg-primary px-4 flex flex-col justify-between items-center rounded-[5px] shadow-[0px_0px_19px_0px_rgba(0,0,0,0.18)]">
           <div className="flex flex-col gap-[23px] items-center">
             <Typography
               as="h2"
@@ -18,12 +18,12 @@ export default function GetBestUpdates() {
             </Typography>
             <div className="bg-secondary w-[50px] h-[3px]" />
           </div>
-          <p className="text-center text-size-18 leading-28-5 tracking-0 font-family-inter mt-[25px] text-secondary">
+          <p className="text-center text-size-18 leading-28-5 tracking-0 font-family-inter mt-[25px] text-secondary max-w-[540px]">
             {data.updates.desc}
           </p>
           <form
             action="#"
-            className="flex gap-4 items-center justify-center w-full mt-[49px]"
+            className="flex gap-4 items-start justify-center w-full mt-[47px]"
             aria-label="Subscribe to updates"
           >
             <input
@@ -34,14 +34,14 @@ export default function GetBestUpdates() {
               autoComplete="email"
               required
               aria-label="Email address"
-              className="w-full h-[70px] px-[39px] pt-[19px] pb-[20px] border border-denary max-w-[656px] bg-white placeholder-quinary text-black"
+              className="w-full h-[70px] px-[38px] pt-5 pb-[19px] border border-denary max-w-[656px] bg-white placeholder-quinary placeholder:text-size-18 text-black"
             />
             <button
-              className="bg-secondary w-full flex justify-center items-center max-w-[328px] h-[70px]"
+              className="bg-secondary w-full flex justify-center items-center max-w-[328px] h-[68px]"
               type="submit"
               aria-label="Subscribe"
             >
-              <span className="text-size-18 leading-30-4 tracking-0-18 text-white font-family-inter">
+              <span className="text-size-18 leading-30-4 tracking-0-18 text-white font-family-inter mt-0.5">
                 {data.updates.cta}
               </span>
             </button>
